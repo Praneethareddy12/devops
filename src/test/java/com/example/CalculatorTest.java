10f0046883f0d0eb2737e22bf95b07c5d450d9ca
Pranithareddy12 <praneethareddy555@gmail.com>
2025-07-03 11:04:43 +0530

    bdjb

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -7,8 +7,15 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calc = new Calculator();
-        int result = calc.add(2, 3);
+        int result = calc.add(3, 2);
         System.out.println("Test result: " + result); // This will appear in Console Output
         assertEquals(5, result);
+    }
+    @Test
+    public void testsubtract() {
+        Calculator calc1 = new Calculator();
+        int result1 = calc.subtract(3, 2);
+        System.out.println("Test result: " + result1);
+        assertEquals(1, result1);
     }
 }
